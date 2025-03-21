e66d717d651c9ff29f927f6241fb572fa8110d38
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-03-21 14:27:54 +0530

    done

--- script/script.js
+++ script/script.js
@@ -1,10 +1,34 @@
-// --------- 
+// ----------- LOADER ANIMATION ------------
+
+const content = document.querySelectorAll(".main-content");
+const loader = document.getElementById("loader");
+
+content.forEach((content) => {
+  content.style.display = "none";
+
+});
+
+window.addEventListener("load", () => {
+  
+  setTimeout(() => {
+    loader.style.display = "none";
+
+    content.forEach((content) => {
+      content.style.display = "block";
+
+    });
+
+  }, 3000);
+});
+
+// ----------- LOADER ANIMATION ------------
+
+// --------- NAVBAR ----------
 
 let lastScrollY = window.scrollY;
 const header = document.getElementById("header");
 
 window.addEventListener("scroll", () => {
-
   if (window.scrollY > lastScrollY) {
 
     header.style.top = "-70px";
@@ -20,6 +44,8 @@ window.addEventListener("scroll", () => {
   lastScrollY = window.scrollY;
 });
 
+// --------- NAVBAR ----------
+
 // ----------- TOP BTN --------------
 
 const topBtn = document.getElementById("topBtn");
@@ -27,27 +53,24 @@ const topBtn = document.getElementById("topBtn");
 topBtn.style.display = "none";
 window.addEventListener("scroll", () => {
 
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
 
-    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+    topBtn.style.display = "block";
+    topBtn.style.transition = "all 0.2s";
 
-         topBtn.style.display = "block";
-         topBtn.style.transition = "all 0.2s";
+  } else {
 
-    }else{
+    topBtn.style.display = "none";
+    topBtn.style.transition = "all 0.2s";
 
-        topBtn.style.display = "none";
-        topBtn.style.transition = "all 0.2s";   
-
-    }
-
-})
+  }
+});
 
 topBtn.addEventListener("click", () => {
+  scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
-    scrollTo({
-
-        top: 0,
-        behavior:"smooth"
-
-    })
-})
+// ----------- TOP BTN --------------
